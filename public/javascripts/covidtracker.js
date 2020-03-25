2364d7379fdd2cdc7336736c6241f3a5b3d40006
alek <alek.br@gmail.com>
2020-03-24 23:22:32 -0700

    update

--- public/javascripts/covidtracker.js
+++ public/javascripts/covidtracker.js
@@ -7,7 +7,9 @@ var countrySynonyms = {
 	"Taiwan*" : "Taiwan",
 	"Iran (Islamic Republic of)": "Iran",
 	"Taipei and environs": "Taiwan",
-	"occupied Palestinian territory": "Israel"
+	"occupied Palestinian territory": "Israel",
+	"Viet Nam": "Vietnam",
+	"Russian Federation": "Russia"
 }
 
 //
@@ -57,7 +59,6 @@ function getColor(intensity) {
 	return "rgba(" + scale[Math.min(scale.length-1, Math.floor(intensity/1000))] + "," + (0.2 + intensity/100)  +")"
 }
 
-
 //
 // grid init etc.
 //
@@ -67,14 +68,20 @@ $(document ).ready(function() {
 	var start = new Date()
 
 	// add the container grid
+
+	// var cellSize = window.innerWidth/(data.length*2.5)
+	var cellIncrement = 80/(data.length*1.2)
+	var cellSize = (cellIncrement/100)*window.innerWidth
 	
 	var grid = $("<div>").attr('id', 'grid')
-	grid.css({"grid-template-columns": "repeat(" + data.length + ", 8px) 20%"})
-	$("body").append(grid)
+	grid.css({"grid-template-columns": "repeat(" + (data.length-1) + ", "  + cellIncrement + "%) 20%"})
+	$("#grid-container").append(grid)
 
 	// aggregate counts per location
 
-	var gridCounts = {}
+	var confirmedCounts = {}
+	var deathCounts = {}
+	var recoveredCounts = {}
 
 	for (var i=0; i<data.length; i++) {
 		var date = data[i]['date']
@@ -84,20 +91,33 @@ $(document ).ready(function() {
 			if (exclusionList.includes(location)) {
 				continue
 			}
-			if (!gridCounts[location]) {
-				gridCounts[location] = new Array(data.length).fill(0);
+			if (!confirmedCounts[location]) {
+				confirmedCounts[location] = new Array(data.length).fill(0);
+				deathCounts[location] = new Array(data.length).fill(0);
+				recoveredCounts[location] = new Array(data.length).fill(0);
 			}
 			if (entries[j]["Confirmed"]) {
-				gridCounts[location][i] += parseInt(entries[j]["Confirmed"])
+				confirmedCounts[location][i] += parseInt(entries[j]["Confirmed"])
+			}
+			if (entries[j]["Deaths"]) {
+				deathCounts[location][i] += parseInt(entries[j]["Deaths"])
+			}
+			if (entries[j]["Recovered"]) {
+				recoveredCounts[location][i] += parseInt(entries[j]["Recovered"])
 			}
 		}
 	}
 
 	// render grid
-	
-	for (location in gridCounts) {
-		for (var i=0; i<gridCounts[location].length; i++) {
-			var confirmed = gridCounts[location][i]
+
+	var rendered = 0	
+	for (location in confirmedCounts) {
+		if (rendered++ > 100) {
+			break
+		}
+		for (var i=1; i<confirmedCounts[location].length; i++) {
+			// var confirmed = gridCounts[location][i]
+			var confirmed = (confirmedCounts[location][i] - confirmedCounts[location][i-1])
 			var dim = Math.min(1+confirmed/100,8) + "px"
 
 			var entry = $("<div>").addClass("grid-entry")
@@ -105,7 +125,19 @@ $(document ).ready(function() {
 			$("#grid").append(entry)
 			lastConfirmed = confirmed
 		}
-		var entry = $("<div>" + location + "</div>").addClass("row-label").css({"color": getColor(gridCounts[location].slice(-1)[0])})
+		var lastVal = confirmedCounts[location].slice(-1)
+		var entry = $("<div>" + location + " (" + lastVal.toLocaleString() + ")</div>").addClass("row-label").attr("location",location).css({"color": getColor(lastVal)})
+
+		entry.hover(function() { 
+			let l =  $(this).attr("location")
+			let lastCount = confirmedCounts[l].slice(-1)
+			$("#metadata-container").empty()
+			$("#metadata-container").append("<h2>" + l + "</h2>")
+									.append("<h4><b>" + confirmedCounts[l].slice(-1).toLocaleString() + "</b> cases confirmed</h4>")
+									.append("<h4><b>" + deathCounts[l].slice(-1).toLocaleString() + "</b> deaths</h4>")
+									.append("<h4><b>" + recoveredCounts[l].slice(-1).toLocaleString() + "</b> recovered</h4>")
+		})
+
 		$("#grid").append(entry)		
 	}
 
